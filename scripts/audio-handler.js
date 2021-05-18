3351a18513cfc3c6a39bdf029ac1e02051433b3a
wsteo <35697328+wsteo@users.noreply.github.com>
2021-05-18 21:29:56 +0800

    Implement play audio feature when marker is found

--- scripts/audio-handler.js
+++ scripts/audio-handler.js
@@ -2,18 +2,29 @@ AFRAME.registerComponent("audiohandler",{
     schema: {
         marker: {type: "string"},
         entity: {type: "string"},
-        //audio: {type: "string", default: ""}
+        audio: {type: "string", default: ""}
     },
 
     update:function(){
-        const _marker = document.querySelector(this.data.marker);
-        const _entity = document.querySelector(this.data.entity);
+        this._marker = document.querySelector(this.data.marker);
+        this._entity = document.querySelector(this.data.entity);
+        this._audio = document.querySelector(this.data.audio);
+    },
+    
+    tick:function(){
+        var _marker = this._marker;
+        var _entity = this._entity;
+        var _audio = this._audio;
 
         if (_marker){
             _marker.addEventListener("markerFound", ()=>{
-                console.log("Marker Found!")
-                console.log(_marker);
-                console.log(_entity);
+                console.log("Marker Found!");
+                _audio.play();
+            });
+
+            _marker.addEventListener("markerLost", ()=>{
+                console.log("Marker Lost!");
+                _audio.pause();
             });
         }
     }
